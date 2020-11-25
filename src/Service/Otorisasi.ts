731b275763efb02e9a4702c4059aad81018cda49
zesagata <zezagata17@gmail.com>
2020-11-25 19:11:18 +0700

    change some variable name

--- src/Service/Otorisasi.ts
+++ src/Service/Otorisasi.ts
@@ -5,27 +5,27 @@ export class Otorisasi {
 
   public static client: Otorisasi
 
-  private constructor(public creds: OtorisasiCredentialsMaping) {}
+  private constructor(public creds: OtorisasiCredentialsMaping, private baseUrl: string) {}
 
   public static async build(baseUrl: string, clientCredential: OtorisasiCredentials[]): Promise<Otorisasi> {
     if (!Otorisasi.client) {
       const creds: OtorisasiCredentialsMaping = {}
       for (const credData of clientCredential) {
         const { clientId, clientSecret } = credData
-        const authResponse = await Otorisasi.getClientToken(clientId, clientSecret, baseUrl)
+        const authResponse = await Otorisasi.requestClientToken(clientId, clientSecret, baseUrl)
         creds[clientId] = {
           clientSecret: clientSecret,
           token: authResponse?.auth?.token,
           expiredAt: new Date(authResponse?.auth?.expiredAfter)
         }
       }
-      Otorisasi.client = new Otorisasi(creds)
+      Otorisasi.client = new Otorisasi(creds, baseUrl)
     }
     return Otorisasi.client
   }
 
 
-  private static async getClientToken(clientId: string, clientSecret: string, baseUrl: string): Promise<any> {
+  private static async requestClientToken(clientId: string, clientSecret: string, baseUrl: string): Promise<any> {
     try {
       const request = createRequest(baseUrl, {}, {})
       const response = await request.post<any>('/api/v1/auth/token', {
